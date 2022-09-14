03cb77a9a507802e2845dc14cb6fd20410a10f86
josedaesbarBitz <josedaesbar@gmail.com>
2022-09-13 19:17:01 -0500

    feat: added endpoints auth and getsessiondata

--- src/controllers/auth/get-session-data.controller.ts
+++ src/controllers/auth/get-session-data.controller.ts
@@ -1,12 +1,14 @@
 import "../../firebase";
 
-import { ControllerBase } from "../../utils/controller";
+import { getAuth } from "firebase-admin/auth";
 
+import { ControllerBase } from "../../utils/controller";
 import { ERROR_CODE_TOKEN_INVALID } from "../../utils/exception-code-responses";
 import { decodeToken } from "../../utils/token";
+import { JwtPayload } from "jsonwebtoken";
 
 type PayloadType = {
-  data: any;
+  user: any;
 };
 
 type RequestBody = {
@@ -19,12 +21,20 @@ const getSessionData = ControllerBase<PayloadType>(async (req, res) => {
   try {
     if (!token) return ERROR_CODE_TOKEN_INVALID;
 
-    const data = decodeToken(token);
+    const data = decodeToken(token) as JwtPayload;
+
+    const auth = getAuth();
+    const userData = await auth.getUserByEmail(data.email);
 
     return {
       code: 200,
       message: "Token valido",
-      payload: { data: data },
+      payload: {
+        user: {
+          uid: userData.uid,
+          email: userData.email,
+        },
+      },
     };
   } catch (error: any) {
     console.log(error.message);
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import validateTokenMiddleware from "../middlewares/validate-token";
-import signin from "../controllers/auth/signin.controller";
+import login from "../controllers/auth/login.controller";
 import getSessionData from "../controllers/auth/get-session-data.controller";
 
 const router = Router();
 
-// router.post("/", validateTokenMiddleware, signin);
-router.post("/", signin);
+// router.post("/", validateTokenMiddleware, login);
+router.post("/", login);
 router.post("/get-session-data", getSessionData);
 
 export default router;
