6c8131382201e85adfb16b926351acda86dddc2b
josedaesbarBitz <josedaesbar@gmail.com>
2022-09-20 21:24:14 -0500

    fix: moved expires token time on constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,5 @@
 import { FIREBASE_API_KEY } from "./enviroment";
 
 export const AUTH_API_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`;
+
+export const TokenExpiresTime = "7d";
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 
 import { UserToken } from "../types/token";
 import { JWT_SECRET_KEY } from "../config/enviroment";
+import { TokenExpiresTime } from "../config/constants";
 
 dotenv.config();
 
 export const generateToken = (payload: UserToken) => {
   const token = jwt.sign(payload, JWT_SECRET_KEY as string, {
-    expiresIn: "7d",
+    expiresIn: TokenExpiresTime,
   });
   return token;
 };
